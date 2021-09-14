e4082edcb4708bc725e58c728352a3331c81818b
tushar <tusharsood44@gmail.com>
2021-09-15 00:09:04 +0530

    my first commit

---
+++ calc/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from . import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add', views.add, name='add'),
+    path('sub', views.sub, name='sub')
+
+]
